3cebbbd87752f72838deec8f142dbbb88e3b6173
tada195908 <48470775+tada195908@users.noreply.github.com>
2021-06-29 18:11:48 +0900

    Add files via upload

--- src/utils/Gnuplot.java
+++ src/utils/Gnuplot.java
@@ -6,27 +6,34 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import myLib.FileIO;
+import myLib.utils.FileIO;
 
 /**
+ * gnuplotを用いて、直接作図しPDFを生成する
  *
  * @author tadaki
  */
 public class Gnuplot {
 
     public static final String nl = System.getProperty("line.separator");
+    private static final String GnuplotCommand="/opt/homebrew/bin/gnuplot";
     private Process process;
     private final BufferedWriter out;
     private final BufferedReader err;
 
-    public Gnuplot() throws IOException {
-        this(".");
-    }
-
+    /**
+     * 指定したディレクトリに出力
+     *
+     * @throws IOException
+     */
     public Gnuplot(String dir) throws IOException {
         out = openGnuplot(dir);
         err = new BufferedReader(
                 new InputStreamReader(process.getErrorStream()));
+    }
+
+    public Gnuplot() throws IOException {
+        this(".");
     }
 
     /**
@@ -36,18 +43,18 @@ public class Gnuplot {
      * @return
      * @throws IOException
      */
-    private BufferedWriter openGnuplot()
-            throws IOException {
-        return openGnuplot(".");
-    }
-
     private BufferedWriter openGnuplot(String dir)
             throws IOException {
-        ProcessBuilder processBuilder = new ProcessBuilder("gnuplot.exe").
+        ProcessBuilder processBuilder = new ProcessBuilder(GnuplotCommand).
                 directory(new File(dir));
         process = processBuilder.start();
         return new BufferedWriter(
                 new OutputStreamWriter(process.getOutputStream()));
+    }
+
+    private BufferedWriter openGnuplot()
+            throws IOException {
+        return openGnuplot(".");
     }
 
     public void readHeader(String filename) throws IOException {
@@ -91,14 +98,21 @@ public class Gnuplot {
      * @throws java.io.IOException
      */
     public static void main(String args[]) throws IOException {
-        String dir = "work";//作業ディレクトリの指定
+        String dir = ".";//作業ディレクトリの指定
         Gnuplot gnuplot = new Gnuplot(dir);
-        //gnuplotスクリプトの最初の部分を読み込み
-        gnuplot.readHeader(dir + File.separator + "sample-template.plt");
-        //出力ファイル指定
-        gnuplot.setOutput("sample.eps");
-        try (BufferedWriter outG = gnuplot.getWriter()) {
-            outG.write("set xrange[0:20]");
+        String header[]={
+            "set terminal pdfcairo enhanced color solid "
+            + "size 29cm,21cm font \"Times-New-Roman\" fontscale 1.2",
+            "set xrange [0:20]",
+            "set yrange [-1:1]",
+            "set title \"TITLE\""
+        };
+        try ( BufferedWriter outG = gnuplot.getWriter()) {
+            for(String s:header){
+                outG.write(s);
+                outG.newLine();
+            }
+            outG.write("set output \"sample.pdf\"");
             outG.newLine();
             outG.write("plot \"-\" title \"data\", sin(x) with line notitle");
             //出力データをgnuplotヘ送る
